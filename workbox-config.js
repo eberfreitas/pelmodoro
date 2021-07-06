79cf15d117db020ae731eda54e0bda315dadd0c6
Éber F. Dias <eber.freitas@gmail.com>
2021-07-06 17:06:06 -0300

    Adds wav files to cache and increases file limit size

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,12 @@
 module.exports = {
 	globDirectory: 'dist/',
 	globPatterns: [
-		'**/*.{html,js,css,png,svg,jpg,gif,json,woff,woff2,eot,ico,webmanifest,map}'
+		'**/*.{html,js,css,png,svg,jpg,gif,json,woff,woff2,eot,ico,webmanifest,map,wav}'
 	],
 	ignoreURLParametersMatching: [
 		/^utm_/,
 		/^fbclid$/
 	],
-	swDest: 'dist/sw.js'
+	swDest: 'dist/sw.js',
+	maximumFileSizeToCacheInBytes: 3145728
 };
