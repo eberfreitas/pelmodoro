864d3130a36aeb59fa37d292bc81bbd55d114e70
Éber F. Dias <eber.freitas@gmail.com>
2022-07-04 23:13:11 -0300

    Javascript setFlash fix

--- src/js/helpers/flash.js
+++ src/js/helpers/flash.js
@@ -1,3 +1,3 @@
-export default function (app, title, msg) {
-  app.ports.gotFlashMsg.send({ title: title, msg: msg })
+export default function (app, msg) {
+  app.ports.gotFlashMsg.send({ msg: msg })
 }
