c68d62ecce4deb6b85b43f1b0d1037d1a9e986c7
Éber Freitas Dias <eber.freitas@gmail.com>
2023-02-11 00:35:23 -0300

    Using sentry environments

--- src/js/monit.js
+++ src/js/monit.js
@@ -4,5 +4,6 @@ import { BrowserTracing } from "@sentry/tracing";
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
   integrations: [new BrowserTracing()],
-  tracesSampleRate: parseFloat(import.meta.env.VITE_SENTRY_SAMPLE_RATE)
+  tracesSampleRate: parseFloat(import.meta.env.VITE_SENTRY_SAMPLE_RATE),
+  environment: import.meta.env.VITE_ENVIRONMENT,
 });
