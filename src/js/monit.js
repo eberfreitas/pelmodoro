ec55cfc8232ad27ba5562dfd874b684261f2586d
Éber Freitas Dias <eber.freitas@gmail.com>
2023-02-11 00:23:16 -0300

    Updating sentry monit packages

--- src/js/monit.js
+++ src/js/monit.js
@@ -1,8 +1,8 @@
 import * as Sentry from "@sentry/browser";
-import { Integrations } from "@sentry/tracing";
+import { BrowserTracing } from "@sentry/tracing";
 
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
-  integrations: [new Integrations.BrowserTracing()],
+  integrations: [new BrowserTracing()],
   tracesSampleRate: parseFloat(import.meta.env.VITE_SENTRY_SAMPLE_RATE)
 });
